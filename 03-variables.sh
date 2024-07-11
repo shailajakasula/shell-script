d9ef9653a58e53c433e567d74fa52139faa7cb7f
shailajakasula <shylukasula24@gmail.com>
2024-07-11 13:55:47 +0530

    3rd shell script added

--- 03-variables.sh
+++ 03-variables.sh
@@ -5,6 +5,5 @@ person2=suresh
 
 echo "$person1 :: hello $person2 good mrng"
 echo "$person2 :: hi, $person1 very gd mrng"
-echo "$person1 :: how are you $person2
- "
+echo "$person1 :: how are you $person2"
 echo "$person2:: i am good $person1"
