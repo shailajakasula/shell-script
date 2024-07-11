93edc81c2c198d4f955f354b934d26340ef19c22
shailajakasula <shylukasula24@gmail.com>
2024-07-11 13:42:24 +0530

    added 2nd script

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+person1=ramesh
+person2=suresh
+
+echo "$person1 :: hello $person2 good mrng"
+echo "$person2 :: hi, $person1 very gd mrng"
+echo "$person1 :: how are you $person2
+ "
+echo "$person2:: i am good $person1"
