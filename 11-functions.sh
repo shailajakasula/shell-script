f5adb1a44c68c192e7ce6d3dad9a630a7ecbfe60
shailajakasula <shylukasula24@gmail.com>
2024-07-12 14:49:45 +0530

    added 4nd script

---
+++ 11-functions.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+id=$(id -u)
+
+validate(){
+ if [ $? -ne 0 ] 
+    then 
+        echo " error :: installing my sql failed"
+        exit 1
+    else 
+        echo " installing my sql success"
+ fi
+}
+
+
+
+if [ id -ne 0 ]
+then 
+  echo " error : please run this script root user "
+  exit 1
+ else 
+   echo " root user" 
+fi
+
+yum install mysql -y 
+
+validate
+
+yum install git -y 
+
+VALIDATE
