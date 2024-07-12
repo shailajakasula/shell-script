b949cd1dcd33064a98916362d23f8233fc1bd05a
shailajakasula <shylukasula24@gmail.com>
2024-07-12 15:50:39 +0530

    added 4nd script

---
+++ 12-logs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+id=$(id -u)
+ echo " script name : $0 "
+validate(){
+ if [ $? -ne 0 ] 
+    then 
+        echo " error : $2... failed"
+        exit 1
+    else 
+        echo " $2.... success"
+ fi
+}
+
+
+
+if [ id -ne 0 ]
+then 
+  echo " error : please run this script root user "
+  exit 1
+ else 
+   echo " root user" 
+fi
+
+yum install mysql -y 
+
+validate $? "installing mysql"
+
+yum install git -y 
+
+validate $? "installing git"
