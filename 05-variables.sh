daad55887124c14eff26da211082d238d83579b1
shailajakasula <shylukasula24@gmail.com>
2024-07-12 13:02:44 +0530

    added 4nd script

---
+++ 05-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1 :: hello $person2 good mrng"
+echo "$person2 :: hi, $person1 very gd mrng"
+echo "$person1 :: how are you $person2"
+echo "$person2:: i am good $person1"
