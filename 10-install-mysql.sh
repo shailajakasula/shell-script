6e3ee907946933298c97dca07a9dfcf91573a700
shailajakasula <shylukasula24@gmail.com>
2024-07-12 14:36:29 +0530

    added 4nd script

--- 10-install-mysql.sh
+++ 10-install-mysql.sh
@@ -6,9 +6,17 @@ if [ id -ne 0 ]
 
 then 
   echo " error : please run this script root user "
-
+  
  else 
    echo " root user" 
 fi
 
 yum install mysql -y
+
+if [ $? -ne 0 ] 
+then 
+  echo " error :: installing my sql failed"
+ exit 1
+else 
+  echo " installing my sql success"
+fi
