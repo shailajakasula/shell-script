3240f720cbc3955584716732e90150f92ce0a0ad
shailajakasula <shylukasula24@gmail.com>
2024-07-16 13:17:04 +0530

    added 4nd script

---
+++ 14-ifs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+file= /etc/passwd
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [! -f $file ]
+  then 
+   echo -e "$R source directory : $file does not exist $N"
+fi
+
+while IFS=":" read -r username passwd user_id group_id
+
+do 
+ echo "username : $username"
+ echo "passwd  : $passwd"
+ echo "user_id : $user_id"
+ echo "group_id : $group_id"
+
+ done << $file
